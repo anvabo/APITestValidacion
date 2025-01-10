cdd5c125f7922e266baa2a10418e3b80e9b4bd66
Anvabo <andres.valles@outlook.com>
2025-01-10 06:32:48 +0100

    Ejemplo de binding

---
+++ Controllers/BindingController.cs
@@ -0,0 +1,52 @@
+﻿using APITestValidacion.Models.Binding;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace APITestValidacion.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BindingController : ControllerBase
+    {
+        // GET: api/<BindingController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<BindingController>/5
+        [HttpGet]
+        [Route("RangoFechas")]
+        public string RangoFechas([FromQuery] RangoFecha range)
+        {
+            return "value";
+        }
+
+        // GET api/<BindingController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<BindingController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<BindingController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<BindingController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Models/Binding/RangoFecha.cs
@@ -0,0 +1,36 @@
+﻿namespace APITestValidacion.Models.Binding
+{
+    public class RangoFecha : IParsable<RangoFecha>
+    {
+        public DateOnly? From { get; init; }
+        public DateOnly? To { get; init; }
+
+        public static RangoFecha Parse(string value, IFormatProvider? provider)
+        {
+            if (!TryParse(value, provider, out var result))
+            {
+                throw new ArgumentException("Could not parse supplied value.", nameof(value));
+            }
+
+            return result;
+        }
+
+        public static bool TryParse(string? value,
+                                    IFormatProvider? provider, out RangoFecha dateRange)
+        {
+            var segments = value?.Split(',', StringSplitOptions.RemoveEmptyEntries
+                                           | StringSplitOptions.TrimEntries);
+
+            if (segments?.Length == 2
+                && DateOnly.TryParse(segments[0], provider, out var fromDate)
+                && DateOnly.TryParse(segments[1], provider, out var toDate))
+            {
+                dateRange = new RangoFecha { From = fromDate, To = toDate };
+                return true;
+            }
+
+            dateRange = new RangoFecha { From = default, To = default };
+            return false;
+        }
+    }
+}
